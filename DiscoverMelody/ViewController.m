d40d1d70877f1d7d495972d489881aa095c22836
xiaodw <d.w.xiao@live.com>
2018-04-02 11:31:25 +0800

    域名修改

--- DiscoverMelody/ViewController.m
+++ DiscoverMelody/ViewController.m
@@ -41,11 +41,17 @@ enum APP_DATA_STATUS {
 #define DMLOGIN_STEP_1_DONE @"DmLoginStep1Done"
 #define DMLOGIN_STEP_2_DONE @"DmLoginStep2Done"
 
+#if 1 //测试环境
 #define URL_GET_APP_CONFIG @"http://test.api.cn.discovermelody-app.com/init/getPCConfig"
 #define URL_GET_USER_INFO @"http://test.api.cn.discovermelody-app.com/User/loginPc"
 #define URL_GET_LESSON_INFO_STUDENT @"http://test.api.cn.discovermelody-app.com/Lesson/studentAccess"
 #define URL_GET_LESSON_INFO_TEACHER @"http://test.api.cn.discovermelody-app.com/Lesson/teacherAccess"
-
+#else //线上环境
+#define URL_GET_APP_CONFIG @"http://api.cn.discovermelody-app.com/init/getPCConfig"
+#define URL_GET_USER_INFO @"http://api.cn.discovermelody-app.com/User/loginPc"
+#define URL_GET_LESSON_INFO_STUDENT @"http://api.cn.discovermelody-app.com/Lesson/studentAccess"
+#define URL_GET_LESSON_INFO_TEACHER @"http://api.cn.discovermelody-app.com/Lesson/teacherAccess"
+#endif
 
 @interface ViewController()
 @property (strong,nonatomic) LoginView* loginView;
